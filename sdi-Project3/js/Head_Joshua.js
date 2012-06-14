cf9a1a26d0846c00e78ef695019e956660a6e6e3
Josh <jhead12@fullsail.edu>
2012-06-14 02:46:10 -0700

    updates

--- sdi-Project3/js/Head_Joshua.js
+++ sdi-Project3/js/Head_Joshua.js
@@ -9,8 +9,11 @@ The Studio in detail
 
 
 // Global variables Property and Methods
+var studioA = studio.facility[0].room;
+var studioB = studio.facility[1].room;
+var studioC = studio.facility[2].room;
 
- // Britney Spears
+
 var studioA = {
 		name: "Briney Spears",
 		getHours: function (time) {},
@@ -39,25 +42,44 @@ var studioC = {
 		};
 
 
-for (var key in studioB) {
-	console.log("key: " + key + ", value: " + studioB[key]);
+var data = function (json) {
+	console.log(json);	
+		
+};
+
+data(studio);
+
+// Studio C Needs a Runner!!
+
+if (studioC.runner && studioC.hungry){ 
+	console.log("since there are no runners in this room and there is a runner available " +  studioC.name + " said: Go get some " + studioC.food)}
+
+
+
+if (studioB.runner && studioB.hungry || studioA.runner && studioA.hungry ) {
 	
+	
+	
+	console.log(studioC.name + " said: go get the Runner from " +  studio.facility[1].room)
 }
+
+else
+
+	{console.log( studioC.name + " said: We Need a Runner in Studio C! ")};
+	
+	
+	
+	
+
+var fullGreeting = greetByName("Josh")
+	console.log(fullGreeting)
+
 
 studioA.getHours(5);
 console.log(studioA.getHours);
 
 // JSon Call and for loop
 
-var Data = function (json) {
-	for (var i=0; i < studio.facility.length; i++) {
-		var facility = studio.facility[i];
-		console.log( "The "  + facility.room + " is a " + facility.kind + " room and  the engineer is " + facility.engineer + "." );
-	};
-	
-};
-
- Data(studio);
 
 // 
 var jsonstring = JSON.stringify()
