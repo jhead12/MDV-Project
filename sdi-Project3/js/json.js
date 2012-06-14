fc019ee516a45081affaa0a72915e9a9e9daf51c
Josh <jhead12@fullsail.edu>
2012-06-13 20:22:12 -0700

    Assignment 3

---
+++ sdi-Project3/js/json.js
@@ -0,0 +1,24 @@
+var json = {
+	"facility":  {
+			"StudioA": "Recording / Mix"
+			"board": "SSL",
+			"Mic": ["Sony 800g", "Shure Sm57", "AKG C451"],
+			"preAmp": "Neve 1084"
+				},
+				{
+			"studioB": "Recording",
+			"board": "SSL AWS 900",
+			"Mic": "Neumann U 87",
+			"preAmp": "Manley Slam!"
+				},
+				{
+			"studioC": "Recording",
+			"board":"Euphonix",
+			"Neuman u 87",
+			"preAmp": "Manley Slam!" 
+					
+				},
+	]
+	
+	
+}
