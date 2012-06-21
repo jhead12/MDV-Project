d178c1bef99d90e1076c6168fcff847426c40fb3
Josh <jhead12@fullsail.edu>
2012-06-20 22:30:45 -0700

    Project 4

--- sdi-project4/js/Head_Joshua_assignment4.js
+++ sdi-project4/js/Head_Joshua_assignment4.js
@@ -1,1 +1,95 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+/*
+	Assignment 4
+	Joshua Head
+	SDI 1206
+	
+*/
+
+/*
+String	
+
+    Does a string follow a 123-456-7890 pattern like a phone number?
+    Does a string follow an aaa@bbb.ccc pattern like an email address?
+    Is the string a URL? (Does it start with http: or https:?)
+    Title-case a string (split into words, then uppercase the first letter of each word)
+    Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
+
+Number	
+
+    Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+    Fuzzy-match a number: is the number above or below a number within a certain percent?
+    Find the number of hours or days difference between two dates.
+    Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+
+Arrary	
+
+    Find the smallest value in an array that is greater than a given number
+    Find the total value of just the numbers in an array, even if some of the items are not numbers.
+    Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
+*/
+
+var jLibrary = function () {
+		
+		// CHECK NUMERIC FUNCTION
+		var checkNumeric = function (val) {
+			if (isNaN(val)) {
+				return false;
+				
+			} else{
+				return true;
+				
+			};
+		};
+	
+		
+		// CHECK EMAIL FUNCTION
+		var checkEMAIL = function (val) {
+			var web = [".com", ".net" ,".me"]
+						
+			if (web && "@") {
+				return true;
+					
+			} else{
+				return false;
+				
+			};
+		}
+		
+		
+		// CHECK PHONE NUMBER FUNCTION
+		var checkPhone = function (val) {
+			var first = val.substr(0, 2);
+			var lenght = val.lenght == 6;
+			
+			if ((isNaN(val)) && lenght) {
+				return false;
+				
+			} else{
+				
+				return true;
+			};
+		
+		};
+		
+		// NAME FUNCTION
+		
+		// CHECK URL FUNCTION	
+		
+	
+		// Return Values
+		return {
+			"checkNumeric": checkNumeric,
+			"checkEMAIL": checkEMAIL,
+			"checkPhone": checkPhone
+			
+		};
+		
+	
+};
+  var newLib = new jLibrary();
+
+console.log(newLib.checkNumeric(4));
+console.log(newLib.checkEMAIL("jhead12@live.com"));
+ 
